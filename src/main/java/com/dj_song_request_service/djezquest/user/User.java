f2ac60ccfbd6cadd89a3ce717d85921f1f2acbdd
Marc Jamilano <108918196+mjamilano24@users.noreply.github.com>
2024-11-08 16:58:34 -0800

    created user class
    created user class for dj users

---
+++ src/main/java/com/dj_song_request_service/djezquest/user/User.java
@@ -0,0 +1,38 @@
+package com.dj_song_request_service.djezquest.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String userName;
+    private String email;
+
+    // Constructors, getters, and setters
+    public User() {}
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
